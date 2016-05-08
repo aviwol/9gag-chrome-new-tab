c74813bae96bf21d8e66498c7ccbf31b060c2c63
aviwol <aviwolicki@gmail.com>
2016-05-08 17:48:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var main = function(){
         url: "http://infinigag.k3min.eu/trending",
         success: function(data) {
             if(!data['data'][0]['media']){
-                html_text = '<h1>'+data['data'][0]['caption']+'</h1><img src="'+ data['data'][0]['images']['normal']+'" height="450" width="450" /><br><br><a href="'+data['data'][0]['link']+'">Link to original post</a>';
+                html_text = '<h1>'+data['data'][0]['caption']+'</h1><img src="'+ data['data'][0]['images']['normal']+'" /><br><br><a href="'+data['data'][0]['link']+'">Link to original post</a>';
             }
             else{
                  html_text = '<h1>'+data['data'][0]['caption']+'</h1><video height="450" width="450" autoplay><source src="'+ data['data'][0]['media']['mp4']+'" type="video/mp4"></video><br><br><a href="'+data['data'][0]['link']+'">Link to original post</a>';
