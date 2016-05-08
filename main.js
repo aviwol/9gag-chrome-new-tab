d1f4cb3e3bf5d86cec7fef8e71a980f4a5e00e41
aviwol <aviwolicki@gmail.com>
2016-05-08 16:42:07 +0300

    Added gif/video support

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ var main = function(){
     $.ajax({
         url: "http://infinigag.k3min.eu/trending",
         success: function(data) {
-            $('#9gagcontent').html('<h1>'+data['data'][0]['caption']+'</h1><img src="'+ data['data'][0]['images']['normal']+'" height="450" width="450" /><br><br><a href="'+data['data'][0]['link']+'">Link to original post</a>');
+            if(!data['data'][0]['media']){
+                html_text = '<h1>'+data['data'][0]['caption']+'</h1><img src="'+ data['data'][0]['images']['normal']+'" height="450" width="450" /><br><br><a href="'+data['data'][0]['link']+'">Link to original post</a>';
+            }
+            else{
+                 html_text = '<h1>'+data['data'][0]['caption']+'</h1><video height="450" width="450" autoplay><source src="'+ data['data'][0]['media']['mp4']+'" type="video/mp4"></video><br><br><a href="'+data['data'][0]['link']+'">Link to original post</a>';
+            }
+            $('#9gagcontent').html(html_text);
         }   
     });
 }
