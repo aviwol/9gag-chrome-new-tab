bf4e36b83bb1cbb82fc6af1c5a1b40ca7587e977
aviwol <aviwolicki@gmail.com>
2016-05-11 18:01:56 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ var main = function(){
     $.ajax({
         url: "http://infinigag.k3min.eu/trending",
         success: function(data) {
-            random_post = data['data'][Math.floor(Math.random() * data['data'].length)];
-            if(!random_post['media']){
-                html_text = '<h1>'+random_post['caption']+'</h1><img src="'+ random_post['images']['normal']+'" /><br><br><a href="'+random_post['link']+'">Link to original post</a>';
+            randomPost = data['data'][Math.floor(Math.random() * data['data'].length)];
+            if(!randomPost['media']){
+                html_text = '<h1>'+randomPost['caption']+'</h1><img src="'+ randomPost['images']['normal']+'" /><br><br><a href="'+randomPost['link']+'">Link to original post</a>';
             }
             else{
-                 html_text = '<h1>'+random_post['caption']+'</h1><video height="450" width="450" autoplay><source src="'+ random_post['media']['mp4']+'" type="video/mp4"></video><br><br><a href="'+random_post['link']+'">Link to original post</a>';
+                 html_text = '<h1>'+randomPost['caption']+'</h1><video height="450" width="450" autoplay><source src="'+ randomPost['media']['mp4']+'" type="video/mp4"></video><br><br><a href="'+randomPost['link']+'">Link to original post</a>';
             }
             $('#9gagcontent').html(html_text);
         }   
